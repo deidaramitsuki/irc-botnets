e8318d514949035d0992483249ea7110cec50423
deidaramitsuki <76633698+deidaramitsuki@users.noreply.github.com>
2021-06-29 19:06:27 -0300

    Add files via upload

---
+++ sadattack.py
@@ -0,0 +1,3 @@
+--2021-06-29 19:06:10--  https://pastebin.com/raw/vrxtgdj5
+Resolving pastebin.com (pastebin.com)... failed: Este host não é conhecido. .
+wget: unable to resolve host address 'pastebin.com'

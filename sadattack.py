4c72188b57235c88cceb190d46869e4283129898
deidaramitsuki <76633698+deidaramitsuki@users.noreply.github.com>
2021-06-29 19:17:55 -0300

    Delete sadattack.py

--- sadattack.py
+++
@@ -1,3 +0,0 @@
---2021-06-29 19:06:10--  https://pastebin.com/raw/vrxtgdj5
-Resolving pastebin.com (pastebin.com)... failed: Este host não é conhecido. .
-wget: unable to resolve host address 'pastebin.com'
